5f687a9b4624459c1db083cd7a4de97d6128c529
omarmt29 <omarmendezt29@gmail.com>
2022-05-27 09:56:26 -0400

    sexto

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import { SimpleGrid, Image, } from '@chakra-ui/react'
+import { SimpleGrid, Image, Flex} from '@chakra-ui/react'
 import { Routes, Route } from 'react-router-dom'
 import { LaunchList } from './Components/LaunchList'
 import { LaunchDetails } from './Components/LaunchDetails'
-
+import Logo from '../src/Assets/Logo.png'
 
 function App() {
 
@@ -11,7 +11,9 @@ function App() {
       <div bgColor='red'>
 
          <SimpleGrid columns={[1, null, 1]} p='0' bgColor='black' justifyContent='center' alignItems='center' w='full' >
-            <Image w='full' src='http://assets.stickpng.com/images/5842a770a6515b1e0ad75afe.png' alt='Dan Abramov' />
+            <Flex justifyContent='center' alignItems='center' paddingLeft='10' paddingTop='5'>
+               <Image w='sm' justifyContent='center' alignItems='center' src={Logo} alt='Dan Abramov' />
+            </Flex>
             <Routes>
                <Route path='/' element={<LaunchList />} />
                <Route path='/launches/:launchId' element={<LaunchDetails />} />
