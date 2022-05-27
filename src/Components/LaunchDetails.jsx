6b52f2731ca2c4484940fb81c76a5bf60bc256f9
omarmt29 <omarmendezt29@gmail.com>
2022-05-27 12:24:36 -0400

    arreglo de texto details

--- src/Components/LaunchDetails.jsx
+++ src/Components/LaunchDetails.jsx
@@ -21,8 +21,8 @@ export function LaunchDetails() {
 
              <Box bgColor='black'>
             <Flex bgColor='blue.800' justifyContent='center' alignItems='center' direction=" column" p='6'  h='auto'>
-                <Text fontSize='xl'>Details</Text>
-                <Text textAlign='center' p='10'>{launch.details}</Text>
+                <Text color='white' fontSize='xl'>Details</Text>
+                <Text color='white' textAlign='center' p='10'>{launch.details}</Text>
                 <Link to='/SpaceX'>
                   <Button w='20' mt='' colorScheme='purple'>Back</Button>
                 </Link>
