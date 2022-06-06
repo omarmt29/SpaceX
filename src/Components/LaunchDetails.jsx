a86d26c411f3cc6f7402062d024c99162400b1f4
omarmt29 <omarmendezt29@gmail.com>
2022-06-06 19:44:09 -0400

    agregar componente de carga

--- src/Components/LaunchDetails.jsx
+++ src/Components/LaunchDetails.jsx
@@ -2,7 +2,7 @@ import { useParams } from "react-router-dom"
 import { useState, useEffect, } from "react"
 import * as API from "../Services/Launches.js";
 import { Text, Flex, Box, Button, SimpleGrid } from '@chakra-ui/react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 export function LaunchDetails() {
 
@@ -19,22 +19,22 @@ export function LaunchDetails() {
         <SimpleGrid columns={[1, null, 1]} spacing='40px' w='full' p='9' bgColor='black' justifyContent='center' alignItems='center'>
 
 
-             <Box bgColor='black'>
-            <Flex bgColor='blue.800' justifyContent='center' alignItems='center' direction=" column" p='6'  h='auto'>
-                <Text color='white' fontSize='xl'>Details</Text>
-                <Text color='white' textAlign='center' p='10'>{launch.details}</Text>
-                <Link to='/SpaceX'>
-                  <Button w='20' mt='' colorScheme='purple'>Back</Button>
-                </Link>
-            </Flex>
+            <Box bgColor='black'>
+                <Flex bgColor='blue.800' justifyContent='center' alignItems='center' direction=" column" p='6' h='auto'>
+                    <Text color='white' fontSize='xl'>Details</Text>
+                    <Text color='white' textAlign='center' p='10'>{launch.details = null ? 'Not found' : launch.details}</Text>
+                    <Link to='/SpaceX'>
+                        <Button w='20' mt='' colorScheme='purple'>Back</Button>
+                    </Link>
+                </Flex>
 
 
 
 
-        </Box>
-        
-        </SimpleGrid> 
-       
+            </Box>
+
+        </SimpleGrid>
+
 
     )
 }
--- src/Components/LaunchList.jsx
+++ src/Components/LaunchList.jsx
@@ -1,23 +1,37 @@
 import * as API from '../Services/Launches.js';
-import {Launchitems} from './Launchitems'
+import { Launchitems } from './Launchitems'
 import { useState, useEffect, } from 'react'
-import { SimpleGrid,} from '@chakra-ui/react'
+import { SimpleGrid, Text, Flex } from '@chakra-ui/react'
 
-export function LaunchList (){
+
+export function LaunchList() {
     const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         API.getallLaunches().then(setData);
-        
+        setLoading(false);
+
     }, [])
 
-    return(
-        <SimpleGrid columns={[1, null, 4]} spacing='40px' w='full' p='9' bgColor='black'>
-        {data.map(item =>
-          <Launchitems key={item.flight_number} {...item} />
+    return (
+        <>
+            {loading ? <Flex flexWrap='wrap' justifyContent='center' alignItems='center' w='full'> <Text color='white' fontWeight='bold'>LOADING..</Text></Flex> :
+                <SimpleGrid columns={[1, null, 4]} spacing='40px' w='full' p='9' bgColor='black'>
+                       {data.map(item =>
+                          <Launchitems key={item.flight_number} {...item} /> )}
+                </SimpleGrid>}
 
-          
-        )}
-     </SimpleGrid> 
+        </>
     )
+
+
+    // {loading ? <SimpleGrid columns={[1, null, 4]} spacing='40px' w='full' p='9' bgColor='black'>
+
+    //             {data.map(item =>
+    //                 <Launchitems key={item.flight_number} {...item} />
+
+
+    //             )}
+    //         </SimpleGrid>}
 }
